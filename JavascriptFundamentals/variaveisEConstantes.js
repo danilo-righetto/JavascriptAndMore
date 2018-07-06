f93e62b9cf8a929f84915e7f941f93efa2686c8a
Danilo Righetto <drighetto@allin.com.br>
2018-07-06 18:06:29 -0300

    Variaveis e Constantes

---
+++ JavascriptFundamentals/variaveisEConstantes.js
@@ -0,0 +1,15 @@
+// Declaração de Variaveis
+var a = 3;
+let b = 4;
+
+var a = 30;
+let b = 40;
+// Existe uma diferença entre criar variaveis entre "let" e "var"
+// Prefira criar variaveis com "let"
+
+console.log(a, b);
+
+/* Veja que não é possível re-declarar variaveis com "let" */
+
+const c = 5; // Constantes possuel valores fixos
+console.log("Constante:"+c);
