b2348b59fa7e069c72d7e34c8b8155650515a936
Danilo Righetto <drighetto@allin.com.br>
2018-07-06 17:52:29 -0300

    Comments in Javascript

---
+++ JavascriptFundamentals/comentario.js
@@ -0,0 +1,15 @@
+console.log("Comentário de código - Javascript");
+
+// Comentário de uma linha
+
+/* Comentário de multiplas linhas */
+
+/*
+ * Comentario
+ * de
+ * Multiplas
+ * Linhas
+ * 
+ */
+
+
