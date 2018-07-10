f2d3e81f779707717812150fb3adc6616844d823
Danilo Righetto <drighetto@allin.com.br>
2018-07-10 13:04:47 -0300

    Tipagem Fraca

--- JavascriptFundamentals/variaveisEConstantes.js
+++ JavascriptFundamentals/variaveisEConstantes.js
@@ -3,7 +3,7 @@ var a = 3;
 let b = 4;
 
 var a = 30;
-let b = 40;
+let ba = 40;
 // Existe uma diferença entre criar variaveis entre "let" e "var"
 // Prefira criar variaveis com "let"
 
@@ -13,3 +13,12 @@ console.log(a, b);
 
 const c = 5; // Constantes possuel valores fixos
 console.log("Constante:"+c);
+
+// Entendo algumas tipagens do Javascript
+let qualquer = "legal";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nomes genericos e siglas para variaveis
+let valor = '';
+let numero = 1;
