4e06d2c46be855eb9d18faa3cc204c1f7205a3aa
Danilo Righetto <drighetto@allin.com.br>
2018-08-23 14:17:51 -0300

    Usando VAR em loop - 02 - Parte 2

--- JavascriptFundamentals/usandoVarEmLoop2.js
+++ JavascriptFundamentals/usandoVarEmLoop2.js
@@ -5,7 +5,7 @@ const funcs = [];
 for (var i = 0; i < 10; i++) {
     funcs.push(function (){
         //Adicionar funções no Array e imprimir i
-        console.log(i);
+        console.log('VAR - valor de i = ',i);
     });
 }
 
@@ -13,3 +13,17 @@ funcs[2](); // Mostra o valor da função do indice 2
 funcs[8](); // Mostra o valor da função do indice 8
 
 // Reposta: Nos dois casos vai ser impresso o valor de 10 por conta do escopo de VAR
+
+const funcs2 = [];
+
+for (let i = 0; i < 10; i++) {
+    funcs2.push(function (){
+        //Adicionar funções no Array e imprimir i
+        console.log('LET - valor de i = ',i);
+    });
+}
+
+funcs2[2](); // Mostra o valor da função do indice 2
+funcs2[8](); // Mostra o valor da função do indice 8
+
+// Resposta: para cada indice será impresso um valor diferente
