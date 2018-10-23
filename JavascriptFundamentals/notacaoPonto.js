081cf030282696d11811295c99b1fea9c48eace6
Danilo Righetto <danilonewtrue@gmail.com>
2018-10-23 17:23:07 -0300

    Notação Ponto

---
+++ JavascriptFundamentals/notacaoPonto.js
@@ -0,0 +1,26 @@
+//Notação Ponto
+
+console.log(typeof console);
+
+console.log(Math.ceil(6.1));
+
+const obj1 = {}
+obj1.nome = 'Bola';
+// obj1['nome'] = 'Bola2';
+
+console.log(obj1.nome);
+
+function Obj(nome){
+    this.nome = nome;
+    this.exec = function(){
+        console.log('Exec ...');
+    }
+}
+
+
+const obj2 = new Obj("Cadeira");
+const obj3 = new Obj("Cadeiras");
+
+console.log(obj2);
+console.log(obj3);
+obj3.exec();
