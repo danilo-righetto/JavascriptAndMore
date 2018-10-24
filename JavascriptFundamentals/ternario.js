d1872fb49ba227325ecb8e38dd850525a4df897d
Danilo Righetto <danilonewtrue@gmail.com>
2018-10-24 18:26:21 -0300

    Operador Ternario

---
+++ JavascriptFundamentals/ternario.js
@@ -0,0 +1,17 @@
+//Operador Ternário
+
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado';
+
+console.log(resultado(7.1));
+console.log(resultado(6.1));
+
+console.log(typeof resultado);
+
+const status = nota >= 7 ? 'Aprovado' : 'Reprovado';
+
+/* O Operador Ternário é composto por 3 partes
+- A expressão que vai retornar verdadeiro ou falo
+- A primeira ação caso o resultado seja verdadeiro
+- A Segunda ação para caso o resultado seja falso */
+
+/*  */
