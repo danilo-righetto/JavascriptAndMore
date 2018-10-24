d926f58457ffba92b31d90a1f3c9eb60a91e3233
Danilo Righetto <danilonewtrue@gmail.com>
2018-10-24 18:13:12 -0300

    Operadores Unitarios

---
+++ JavascriptFundamentals/unitarios.js
@@ -0,0 +1,12 @@
+//Operadores Unitarios
+
+let num1 = 1;
+let num2 = 2;
+
+num1++; // forma pós-fixada
+console.log(num1);
+--num1; // forma pre-fixada
+console.log(num1);
+
+console.log(++num1 === num2--); //Verdadeiro
+console.log(num1 === num2);
