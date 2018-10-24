d2f60b861cbf5132a4cbb37d29b0de76fc2d3823
Danilo Righetto <danilonewtrue@gmail.com>
2018-10-24 09:52:04 -0300

    atribuição

---
+++ JavascriptFundamentals/atribuicao.js
@@ -0,0 +1,14 @@
+// Operadores de Atribuição
+
+const a = 7;
+let b = 3;
+b += a; // atribuição aditiva b = b + a
+console.log(b);
+b -= 4; // atribuição subtrativa b = b - 4
+console.log(b);
+b *= 2; // atribuição multiplicativa b = b * 2
+console.log(b);
+b /= 2; // atribuição divisiva b = b / 2
+console.log(b);
+b %= 2; // atribuição modular b = b % 2
+console.log(b);
