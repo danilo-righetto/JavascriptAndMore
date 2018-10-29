8436ce37c612e1eb540b7a81f4a2becac3b85145
Danilo Righetto <danilonewtrue@gmail.com>
2018-10-29 13:28:16 -0300

    Parametros Variaveis

---
+++ Function/paramsVariaveis.js
@@ -0,0 +1,15 @@
+//Parametros Variaveis
+function soma(){
+    let soma = 0;
+    for(i in arguments){
+        soma += arguments[i];
+    }
+    return soma;
+}
+
+console.log(soma());
+console.log(soma(1));
+console.log(soma(1.1, 2.2, 3.3));
+
+console.log(soma('Vamos .. ', 2.2, 'teste'));
+
