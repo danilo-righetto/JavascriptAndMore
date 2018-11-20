90be4ae433600170623d06585bda535afb88173b
Danilo Righetto <danilonewtrue@gmail.com>
2018-11-20 11:59:04 -0200

    iife - Executando Imediatamente

---
+++ Function/iife.js
@@ -0,0 +1,6 @@
+// IIFE -> Expressão de Função - Immediately Invoked Function Expression (Imediatamente Invocada)
+
+(function() {
+    console.log('Será executada na hora');
+    console.log('Foge do escopo mais abrangente');
+})();
