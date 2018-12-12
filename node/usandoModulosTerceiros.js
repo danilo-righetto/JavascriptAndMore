8b33a32e9423f026a32608f39d8bb89020fd8bd7
Danilo Righetto <danilonewtrue@gmail.com>
2018-12-12 17:13:27 -0200

    Usando Módulos de Terceiros

---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,6 @@
+// underline para referenciar o modulo lodash
+const _ = require('lodash')
+setInterval(() => console.log(_.random(1, 1000)), 2000)
+
+// Instale o pacote: nodemon
+/* sudo npm i -g nodemon */
