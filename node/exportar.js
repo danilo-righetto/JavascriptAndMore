ddf1e0b0c129e1cb61525acbc45f6ab030ac9d66
Danilo Righetto <danilonewtrue@gmail.com>
2018-12-14 11:36:42 -0200

    Sistema de Modulos - Exports

---
+++ node/exportar.js
@@ -0,0 +1,18 @@
+// Sistema de Módulos - Exports
+console.log(module.exports === this)
+console.log(module.exports === exports)
+
+this.a = 1
+exports.b = 2
+module.exports.c = 3
+
+exports = null
+console.log(module.exports)
+
+exports = {
+    nome: 'teste do exports'
+}
+
+console.log(module.exports)
+
+module.exports = { publico: true }
